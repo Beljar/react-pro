2b1ebdc7e6e4b0aa7315f4e2d6c23fb044cabe14
unknown <ormallen@gmail.com>
2026-07-26 23:58:18 +0300

    fix: imports

--- src/features/refExamples/ui/WebSocketLogger.tsx
+++ src/features/refExamples/ui/WebSocketLogger.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const WebSocketLogger = () => {
   const socketRef = useRef<WebSocket | null>(null);
