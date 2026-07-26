1ca1526034bb449338743827bcced765b32d6f8d
unknown <ormallen@gmail.com>
2026-07-27 00:06:26 +0300

    fix: click timer

--- src/features/refExamples/ui/ClickTimer.tsx
+++ src/features/refExamples/ui/ClickTimer.tsx
@@ -7,16 +7,25 @@ export const ClickTimer = () => {
     startTime: null,
     clickCount: 0,
   });
+
+  const clickHandler = () => {
+    if (clickRef.current.startTime === null) {
+      clickRef.current.startTime = Date.now();
+    }
+    clickRef.current.clickCount += 1;
+    console.log(
+      `Clicks: ${clickRef.current.clickCount}, Time passed: ${Date.now() - clickRef.current.startTime}ms`
+    );
+  };
   useEffect(() => {
-    document.addEventListener('click', () => {
-      if (clickRef.current.startTime === null) {
-        clickRef.current.startTime = Date.now();
-      }
-      clickRef.current.clickCount += 1;
-      console.log(
-        `Clicks: ${clickRef.current.clickCount}, Time passed: ${Date.now() - clickRef.current.startTime}ms`
-      );
-    });
+    document.addEventListener('click', clickHandler);
+    return () => {
+      document.removeEventListener('click', clickHandler);
+      clickRef.current = {
+        startTime: null,
+        clickCount: 0,
+      };
+    };
   }, []);
   return <></>;
 };
