f32897102c44a52906ebbcb81549eea72d17cb89
unknown <ormallen@gmail.com>
2026-08-10 03:46:53 +0300

    feat: prod and dev configs

--- configs/esbuild.app.prod.config.ts
+++ configs/esbuild.app.prod.config.ts
@@ -3,7 +3,10 @@ import path from 'path';
 
 esbuild
   .build({
-    entryPoints: [path.resolve(process.cwd(), 'app', 'index.ts')],
+    entryPoints: [
+      path.resolve(process.cwd(), 'app', 'index.ts'),
+      path.resolve(process.cwd(), 'index.html'),
+    ],
     bundle: true,
     outdir: 'dist',
     jsx: 'automatic',
